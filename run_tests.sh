e2ff0435b24a74545f368ab2b093c6ad499a41b2
Michael Wiencek <mwtuea@gmail.com>
2022-05-27 02:11:03 -0500

    run_tests.sh: add --if-exists to dropdb

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo -u postgres dropdb musicbrainz_test_artwork_indexer
+sudo -u postgres dropdb --if-exists musicbrainz_test_artwork_indexer
 createdb -O musicbrainz -T musicbrainz_test -U postgres musicbrainz_test_artwork_indexer
 
 psql -U musicbrainz -d musicbrainz_test_artwork_indexer -f sql/create.sql
