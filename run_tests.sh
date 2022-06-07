7b3994d28cfa4fef843e13b08d2ac09472ed0caf
Michael Wiencek <mwtuea@gmail.com>
2022-06-07 12:15:29 -0500

    Move {create,drop}.sql to {create,drop}_schema.sql

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ set -e
 sudo -u postgres dropdb --if-exists musicbrainz_test_artwork_indexer
 createdb -O musicbrainz -T musicbrainz_test -U postgres musicbrainz_test_artwork_indexer
 
-psql -U musicbrainz -d musicbrainz_test_artwork_indexer -f sql/create.sql
+psql -U musicbrainz -d musicbrainz_test_artwork_indexer -f sql/create_schema.sql
 psql -U musicbrainz -d musicbrainz_test_artwork_indexer -f sql/caa_functions.sql
 psql -U musicbrainz -d musicbrainz_test_artwork_indexer -f sql/eaa_functions.sql
 psql -U musicbrainz -d musicbrainz_test_artwork_indexer -f sql/caa_triggers.sql
