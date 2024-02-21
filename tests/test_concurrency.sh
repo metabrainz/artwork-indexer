metabrainz/artwork-indexer

commit e8b8623795a18bb9835de8f503b1b62cfa48e961

Kill async code
This commit replaces asyncpg with psycopg and aiohttp with requests.

artwork-indexer was designed from the start to take advantage of Python's
asyncio event loop, but that doesn't do much for us except allow us start
processing the next event while waiting for I/O on another event. Which is not
very useful, because we can safely (and easily) just run another
artwork-indexer process, and don't need any additional scalability given the
relatively low throughput of image uploads. The async model also increases the
complexity of the code, particularly with respect to error handling.

indexer.py
@@ -17,18 +17,17 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import argparse
-import asyncio
 import configparser
 import json
 import logging
-import re
 import signal
+import time
 import traceback
 from math import inf
 from textwrap import dedent
 
-import aiohttp
-import asyncpg
+import psycopg
+import requests
 import sentry_sdk
 
 from handlers import EVENT_HANDLER_CLASSES
@@ -42,7 +41,7 @@ from handlers import EVENT_HANDLER_CLASSES
 MAX_ATTEMPTS = 5
 
 
-async def handle_event_failure(pg_conn, event, error):
+def handle_event_failure(pg_conn, event, error):
     logging.error(error)
     logging.error(''.join(traceback.format_tb(error.__traceback__)))
 
@@ -68,32 +67,35 @@ async def handle_event_failure(pg_conn, event, error):
     # this would cause compounding failures at worst, and bypass any
     # delay in processing we have on the existing event.
 
-    await pg_conn.execute(dedent('''
+    pg_conn.execute(dedent('''
         UPDATE artwork_indexer.event_queue eq
         SET state = (
-            CASE WHEN eq.attempts >= $1 OR EXISTS (
+            CASE WHEN eq.attempts >= %(max_attempts)s OR EXISTS (
                 SELECT 1
                 FROM artwork_indexer.event_queue dup
                 WHERE dup.state = 'queued'
                 AND dup.action = eq.action
                 AND dup.message = eq.message
-                AND dup.id != $2
+                AND dup.id != %(event_id)s
                 FOR UPDATE
             ) THEN 'failed' ELSE 'queued' END
         )::artwork_indexer.event_state
-        WHERE eq.id = $2
-    '''), MAX_ATTEMPTS, event['id'])
+        WHERE eq.id = %(event_id)s
+    '''), {
+        'max_attempts': MAX_ATTEMPTS,
+        'event_id': event['id'],
+    })
 
-    await pg_conn.execute(dedent('''
+    pg_conn.execute(dedent('''
         INSERT INTO artwork_indexer.event_failure_reason
             (event, failure_reason)
-        VALUES ($1, $2)
-    '''), event['id'], str(error))
+        VALUES (%(event_id)s, %(error)s)
+    '''), {'event_id': event['id'], 'error': str(error)})
 
     sentry_sdk.capture_exception(error)
 
 
-async def cleanup_events(pg_pool):
+def cleanup_events(pg_conn):
     # Cleanup completed events older than 90 days. We only keep these
     # around in case they help with debugging.
     #
@@ -105,175 +107,170 @@ async def cleanup_events(pg_pool):
     # We don't want to delete queued or running events that are older
     # than 90 days: if this occurs, we'd want to inspect them to find
     # out why they're stuck (ideally before 90 days has passed).
-    deletion_tag = await pg_pool.execute(dedent('''
+    pg_cur = pg_conn.execute(dedent('''
         DELETE FROM artwork_indexer.event_queue
         WHERE state = 'completed'
         AND (now() - created) > interval '90 days'
     '''))
-    deletion_count_match = re.match(r'DELETE ([0-9]+)', deletion_tag)
-    if deletion_count_match:
-        deletion_count = int(deletion_count_match[1])
-        if deletion_count:
-            logging.debug(
-                'Deleted ' + str(deletion_count) + ' event' +
-                ('s' if deletion_count > 1 else '') +
-                ' older than 90 days')
+    if pg_cur.rowcount:
+        logging.debug(
+            'Deleted ' + str(pg_cur.rowcount) + ' event' +
+            ('s' if pg_cur.rowcount > 1 else '') +
+            ' older than 90 days')
 
 
-async def run_event_handler(pg_pool, event, handler, message):
-    async with pg_pool.acquire() as pg_conn, pg_conn.transaction():
-        handler_method = getattr(handler, event['action'])
-        try:
-            await handler_method(pg_conn, message)
-        except BaseException as task_exc:
-            await handle_event_failure(pg_conn, event, task_exc)
-        else:
-            logging.debug(
-                'Event id=%s finished succesfully',
-                event['id'],
-            )
-            await pg_conn.execute(dedent('''
-                UPDATE artwork_indexer.event_queue
-                SET state = 'completed'
-                WHERE id = $1
-            '''), event['id'])
+def run_event_handler(pg_conn, event, handler):
+    handler_method = getattr(handler, event['action'])
+    try:
+        handler_method(pg_conn, event['message'])
+    except BaseException as task_exc:
+        handle_event_failure(pg_conn, event, task_exc)
+    else:
+        logging.debug(
+            'Event id=%s finished succesfully',
+            event['id'],
+        )
+        pg_conn.execute(dedent('''
+            UPDATE artwork_indexer.event_queue
+            SET state = 'completed'
+            WHERE id = %(event_id)s
+        '''), {'event_id': event['id']})
 
 
-async def indexer(
+def indexer(
     config,
     maxwait,
     max_idle_loops=inf,
-    http_client_cls=aiohttp.ClientSession
+    http_client_cls=requests.Session
 ):
     sleep_amount = 1  # seconds
 
-    async with \
-            asyncpg.create_pool(**config['database']) as pg_pool, \
-            http_client_cls(raise_for_status=True) as http_session:
-        event_handler_map = {
-            entity: cls(config, http_session)
-            for entity, cls in EVENT_HANDLER_CLASSES.items()
-        }
+    conninfo = psycopg.conninfo.make_conninfo(**config['database'])
+    pg_conn = psycopg.connect(
+        conninfo,
+        autocommit=True,
+        row_factory=psycopg.rows.dict_row
+    )
 
-        idle_loops = 0
+    http_session = http_client_cls()
 
-        while True:
-            await asyncio.sleep(sleep_amount)
+    event_handler_map = {
+        entity: cls(config, http_session)
+        for entity, cls in EVENT_HANDLER_CLASSES.items()
+    }
 
-            async with \
-                    pg_pool.acquire() as pg_conn, \
-                    pg_conn.transaction():
+    idle_loops = 0
 
-                # Skip events that have reached `MAX_ATTEMPTS`.
-                # In other cases, `last_updated` should be within a
-                # specific time interval. We start by waiting 30
-                # minutes, and double the amount of time after each
-                # attempt.
-                event = await pg_conn.fetchrow(dedent('''
-                    SELECT * FROM artwork_indexer.event_queue eq
-                    WHERE eq.state = 'queued'
-                    AND eq.attempts < $1
-                    AND eq.last_updated <=
-                        (now() - (interval '30 minutes' * 2 * eq.attempts))
-                    AND (eq.depends_on IS NULL OR EXISTS (
-                        SELECT TRUE
-                        FROM artwork_indexer.event_queue parent_eq
-                        WHERE array_position(
-                            eq.depends_on,
-                            parent_eq.id
-                        ) IS NOT NULL
-                        AND parent_eq.state = 'completed'
-                    ))
-                    ORDER BY created, id
-                    LIMIT 1
-                    FOR UPDATE SKIP LOCKED
-                '''), MAX_ATTEMPTS)
+    while True:
+        time.sleep(sleep_amount)
 
-                # Reset `sleep_amount` if we're seeing activity, otherwise
-                # increase it exponentially up to `maxwait` seconds.
-                if event:
-                    sleep_amount = 1
-                    idle_loops = 0
-                else:
-                    # Since there's nothing else to do, cleanup old events.
-                    asyncio.create_task(cleanup_events(pg_pool))
+        # Skip events that have reached `MAX_ATTEMPTS`.
+        # In other cases, `last_updated` should be within a
+        # specific time interval. We start by waiting 30
+        # minutes, and double the amount of time after each
+        # attempt.
+        event = pg_conn.execute(dedent('''
+            SELECT * FROM artwork_indexer.event_queue eq
+            WHERE eq.state = 'queued'
+            AND eq.attempts < %(max_attempts)s
+            AND eq.last_updated <=
+                (now() - (interval '30 minutes' * 2 * eq.attempts))
+            AND (eq.depends_on IS NULL OR EXISTS (
+                SELECT TRUE
+                FROM artwork_indexer.event_queue parent_eq
+                WHERE array_position(
+                    eq.depends_on,
+                    parent_eq.id
+                ) IS NOT NULL
+                AND parent_eq.state = 'completed'
+            ))
+            ORDER BY created, id
+            LIMIT 1
+            FOR UPDATE SKIP LOCKED
+        '''), {'max_attempts': MAX_ATTEMPTS}).fetchone()
 
-                    idle_loops += 1
-                    if idle_loops >= max_idle_loops:
-                        break
+        # Reset `sleep_amount` if we're seeing activity, otherwise
+        # increase it exponentially up to `maxwait` seconds.
+        if event:
+            sleep_amount = 1
+            idle_loops = 0
+        else:
+            # Since there's nothing else to do, cleanup old events.
+            cleanup_events(pg_conn)
 
-                    if sleep_amount < maxwait:
-                        sleep_amount = min(sleep_amount * 2, maxwait)
-                        logging.debug(
-                            'No event found; sleeping for %s second(s)',
-                            sleep_amount,
-                        )
+            idle_loops += 1
+            if idle_loops >= max_idle_loops:
+                break
 
-                    continue
+            if sleep_amount < maxwait:
+                sleep_amount = min(sleep_amount * 2, maxwait)
+                logging.debug(
+                    'No event found; sleeping for %s second(s)',
+                    sleep_amount,
+                )
 
-                if event['state'] != 'queued':
-                    # This is mainly a development aid.  In at least one
-                    # occasion I broke the SQL query above by having bad
-                    # boolean operator precedence.  -- mwiencek
-                    raise Exception('Event is not queued: %r', event)
+            continue
 
-                logging.info('Processing event %s', event)
+        if event['state'] != 'queued':
+            # This is mainly a development aid.  In at least one
+            # occasion I broke the SQL query above by having bad
+            # boolean operator precedence.  -- mwiencek
+            raise Exception('Event is not queued: %r', event)
 
-                try:
-                    parsed_message = json.loads(event['message'])
-                except BaseException as e:
-                    await handle_event_failure(pg_conn, event, e)
+        logging.info('Processing event %s', event)
 
-                if event['action'] == 'delete_image' and \
-                        event['depends_on'] is None:
-                    # If `delete_image` event exists with no parent,
-                    # there should be no later `copy_image` event for
-                    # the same image. Verify this to be safe.
-                    later_copy_image_event = await pg_conn.fetchrow(dedent('''
-                        SELECT id FROM artwork_indexer.event_queue eq
-                        WHERE eq.state = 'queued'
-                        AND eq.action = 'copy_image'
-                        AND eq.created > $1
-                        AND eq.message->'artwork_id' = $2
-                        AND eq.message->'old_gid' = $3
-                        AND eq.message->'suffix' = $4
-                        LIMIT 1
-                    '''), event['created'], *[
-                        json.dumps(x) for x in (
-                            parsed_message['artwork_id'],
-                            parsed_message['gid'],
-                            parsed_message['suffix'],
-                        )
-                    ])
+        if event['action'] == 'delete_image' and \
+                event['depends_on'] is None:
+            message = event['message']
+            # If `delete_image` event exists with no parent,
+            # there should be no later `copy_image` event for
+            # the same image. Verify this to be safe.
+            later_copy_image_event = pg_conn.execute(dedent('''
+                SELECT id FROM artwork_indexer.event_queue eq
+                WHERE eq.state = 'queued'
+                AND eq.action = 'copy_image'
+                AND eq.created > %(created)s
+                AND eq.message->'artwork_id' = %(artwork_id)s
+                AND eq.message->'old_gid' = %(gid)s
+                AND eq.message->'suffix' = %(suffix)s
+                LIMIT 1
+            '''), {
+                'created': event['created'],
+                'artwork_id': json.dumps(message['artwork_id']),
+                'gid': json.dumps(message['gid']),
+                'suffix': json.dumps(message['suffix']),
+            }).fetchone()
 
-                    if later_copy_image_event:
-                        await handle_event_failure(
-                            pg_conn,
-                            event,
-                            Exception(
-                                'This image cannot be deleted, because ' +
-                                'a later event exists ' +
-                                f'(id={later_copy_image_event}) ' +
-                                'that wants to copy it.'
-                            )
-                        )
-                        continue
-
-                await pg_conn.execute(dedent('''
-                    UPDATE artwork_indexer.event_queue
-                    SET state = 'running',
-                        attempts = attempts + 1
-                    WHERE id = $1
-                '''), event['id'])
-
-                handler = event_handler_map[event['entity_type']]
-
-                asyncio.create_task(run_event_handler(
-                    pg_pool,
+            if later_copy_image_event:
+                latest_copy_image_event_id = later_copy_image_event['id']
+                handle_event_failure(
+                    pg_conn,
                     event,
-                    handler,
-                    parsed_message,
-                ))
+                    Exception(
+                        'This image cannot be deleted, because ' +
+                        'a later event exists ' +
+                        f'(id={latest_copy_image_event_id}) ' +
+                        'that wants to copy it.'
+                    )
+                )
+                continue
+
+        pg_conn.execute(dedent('''
+            UPDATE artwork_indexer.event_queue
+            SET state = 'running',
+                attempts = attempts + 1
+            WHERE id = %(event_id)s
+        '''), {'event_id': event['id']})
+
+        handler = event_handler_map[event['entity_type']]
+
+        run_event_handler(
+            pg_conn,
+            event,
+            handler,
+        )
+
+    pg_conn.close()
 
 
 def main():
@@ -316,15 +313,7 @@ def main():
         if sentry_dsn:
             sentry_sdk.init(dsn=sentry_dsn)
 
-    loop = None
-    try:
-        loop = asyncio.run(indexer(config, args.maxwait))
-    except KeyboardInterrupt:
-        pass
-    finally:
-        if loop:
-            loop.stop()
-            loop.close()
+    indexer(config, args.maxwait)
 
 
 if __name__ == '__main__':

config.default.ini
@@ -5,7 +5,7 @@ url=https://musicbrainz.org
 host=localhost
 port=5432
 user=musicbrainz
-database=musicbrainz_db
+dbname=musicbrainz_db
 
 [s3]
 url=https://{bucket}.s3.us.archive.org/{file}

config.tests.example.ini
@@ -6,7 +6,7 @@ database=TEST_ARTWORK_INDEXER
 host=localhost
 port=5432
 user=musicbrainz
-database=musicbrainz_test_artwork_indexer
+dbname=musicbrainz_test_artwork_indexer
 
 [s3]
 url=http://{bucket}.s3.example.com/{file}

docker/config.ini.ctmpl
@@ -11,7 +11,7 @@ port={{ .Port }}
 {{- end }}
 {{- end }}
 user={{ keyOrDefault (print $key_prefix "postgres_user") "musicbrainz" }}
-database={{ keyOrDefault (print $key_prefix "postgres_database") "musicbrainz_db" }}
+dbname={{ keyOrDefault (print $key_prefix "postgres_database") "musicbrainz_db" }}
 
 [s3]
 url={{ keyOrDefault (print $key_prefix "s3_url") "https://{bucket}.s3.us.archive.org/{file}" }}

handlers_base.py
@@ -104,21 +104,21 @@ class EventHandler:
         bucket = self.build_bucket_name(gid)
         return url.format(bucket=bucket, file=filename)
 
-    async def fetch_image_rows(self, pg_conn, entity_gid):
+    def fetch_image_rows(self, pg_conn, entity_gid):
         raise NotImplementedError
 
-    async def index(self, pg_conn, message):
+    def index(self, pg_conn, message):
         gid = message['gid']
 
         encoded_index_json = json.dumps({
             'images': [
                 self.build_image_json(gid, row)
-                for row in await self.fetch_image_rows(pg_conn, gid)
+                for row in self.fetch_image_rows(pg_conn, gid)
             ],
             kebab(self.entity_type): self.build_canonical_entity_url(gid),
         }, ensure_ascii=True, sort_keys=True).encode('ascii')
 
-        await self.http_session.put(
+        self.http_session.put(
             self.build_s3_item_url(gid, 'index.json'),
             data=encoded_index_json,
             headers={
@@ -130,32 +130,31 @@ class EventHandler:
                 'x-archive-meta-mediatype': 'image',
                 'x-archive-meta-noindex': 'true',
             },
-        )
+        ).raise_for_status()
 
         entity_metadata_url = self.build_metadata_url(gid)
         entity_metadata_headers = self.build_metadata_headers()
+        metadata_res = self.http_session.get(entity_metadata_url,
+                                             headers=entity_metadata_headers,
+                                             stream=True)
+        metadata_res.raise_for_status()
+        self.http_session.put(
+            self.build_s3_item_url(
+                gid,
+                self.build_metadata_ia_filename(gid),
+            ),
+            data=metadata_res.content,
+            headers={
+                **self.build_authorization_header(),
+                'content-type': 'application/xml; charset=UTF-8',
+                'x-archive-auto-make-bucket': '1',
+                'x-archive-meta-collection': self.ia_collection,
+                'x-archive-meta-mediatype': 'image',
+                'x-archive-meta-noindex': 'true',
+            },
+        ).raise_for_status()
 
-        async with self.http_session.get(entity_metadata_url,
-                                         headers=entity_metadata_headers) \
-                as response:
-            encoded_entity_metadata = (await response.text()).encode('utf-8')
-            await self.http_session.put(
-                self.build_s3_item_url(
-                    gid,
-                    self.build_metadata_ia_filename(gid),
-                ),
-                data=encoded_entity_metadata,
-                headers={
-                    **self.build_authorization_header(),
-                    'content-type': 'application/xml; charset=UTF-8',
-                    'x-archive-auto-make-bucket': '1',
-                    'x-archive-meta-collection': self.ia_collection,
-                    'x-archive-meta-mediatype': 'image',
-                    'x-archive-meta-noindex': 'true',
-                },
-            )
-
-    async def copy_image(self, pg_conn, message):
+    def copy_image(self, pg_conn, message):
         artwork_id = message['artwork_id']
         old_gid = message['old_gid']
         new_gid = message['new_gid']
@@ -180,7 +179,7 @@ class EventHandler:
         )
 
         # Copy the image to the new MBID, and delete the old image.
-        await self.http_session.put(
+        self.http_session.put(
             self.build_s3_item_url(new_gid, new_file_name),
             headers={
                 **self.build_authorization_header(),
@@ -191,9 +190,9 @@ class EventHandler:
                 'x-archive-meta-mediatype': 'image',
                 'x-archive-meta-noindex': 'true',
             },
-        )
+        ).raise_for_status()
 
-    async def delete_image(self, pg_conn, message):
+    def delete_image(self, pg_conn, message):
         gid = message['gid']
 
         filename = IMAGE_FILE_FORMAT.format(
@@ -203,28 +202,28 @@ class EventHandler:
         )
         # Note: This request should succeed (204) even if the file
         # no longer exists.
-        await self.http_session.delete(
+        self.http_session.delete(
             self.build_s3_item_url(gid, filename),
             headers={
                 **self.build_authorization_header(),
                 'x-archive-keep-old-version': '1',
                 'x-archive-cascade-delete': '1',
             },
-        )
+        ).raise_for_status()
 
-    async def deindex(self, pg_conn, message):
+    def deindex(self, pg_conn, message):
         # Note: This request should succeed (204) even if the file
         # no longer exists.
-        await self.http_session.delete(
+        self.http_session.delete(
             self.build_s3_item_url(message['gid'], 'index.json'),
             headers={
                 **self.build_authorization_header(),
                 'x-archive-keep-old-version': '1',
                 'x-archive-cascade-delete': '1',
             },
-        )
+        ).raise_for_status()
 
-    async def noop(self, pg_conn, message):
+    def noop(self, pg_conn, message):
         if message.get('fail'):
             raise Exception('Failure (no-op)')
 
@@ -287,18 +286,18 @@ class MusicBrainzEventHandler(EventHandler):
             headers['mb-set-database'] = database
         return headers
 
-    async def fetch_image_rows(self, pg_conn, mbid):
+    def fetch_image_rows(self, pg_conn, mbid):
         schema = self.artwork_schema
         entity_type = self.entity_type
 
         # Note: cover_art_archive.image_type is also used by the
         # event_art_archive schema.
-        return await pg_conn.fetch(
+        return pg_conn.execute(
             dedent('''
                 SELECT * FROM {schema}.index_listing
                 JOIN cover_art_archive.image_type USING (mime_type)
-                WHERE {entity} = (SELECT id FROM {entity} WHERE gid = $1)
+                WHERE {entity} = (SELECT id FROM {entity} WHERE gid = %(gid)s)
                 ORDER BY ordering
             '''.format(schema=schema, entity=entity_type)),
-            mbid,
-        )
+            {'gid': mbid},
+        ).fetchall()

sql/create_schema.sql
@@ -32,7 +32,7 @@ CREATE TYPE event_state AS ENUM (
     'queued',
     -- 'running' events have started processing and are currently
     -- being handled by the indexer process.  Events generally must
-    -- perform asynchronous database queries and HTTP requests
+    -- perform synchronous database queries and HTTP requests
     -- so may take some time to complete.
     --
     -- If a running event encounters an error, and the value of the

commit 86617019d13f6d37f227072947d4e778a08a4b0c

Fix concurrent event processing
Adding `autocommit=True` in e8b8623795a18bb9835de8f503b1b62cfa48e961 broke the
`SELECT ... FOR UPDATE ... SKIP LOCKED` check we have in place. We have to mark
the event as "running" in the same transaction as the `SELECT`.

I've added a concurrency test to ensure we don't regress here again.

indexer.py
@@ -91,6 +91,8 @@ def handle_event_failure(pg_conn, event, error):
         VALUES (%(event_id)s, %(error)s)
     '''), {'event_id': event['id'], 'error': str(error)})
 
+    pg_conn.commit()
+
     sentry_sdk.capture_exception(error)
 
 
@@ -111,6 +113,7 @@ def cleanup_events(pg_conn):
         WHERE state = 'completed'
         AND (now() - created) > interval '90 days'
     '''))
+    pg_conn.commit()
     if pg_cur.rowcount:
         logging.info(
             'Deleted ' + str(pg_cur.rowcount) + ' event' +
@@ -161,6 +164,7 @@ def run_event_handler(pg_conn, event, handler):
             SET state = 'completed'
             WHERE id = %(event_id)s
         '''), {'event_id': event['id']})
+        pg_conn.commit()
 
 
 def indexer(
@@ -219,6 +223,12 @@ def indexer(
             # boolean operator precedence.  -- mwiencek
             raise Exception('Event is not queued: %r', event)
 
+        # XXX: This is used by tests/test_concurrency.sh to check
+        # that concurrent indexer processes don't select the same
+        # queued events. A small delay is added before we mark them
+        # as `running`.
+        time.sleep(0.25)
+
         logging.info('Processing event %s', event)
 
         pg_conn.execute(dedent('''
@@ -227,6 +237,8 @@ def indexer(
                 attempts = attempts + 1
             WHERE id = %(event_id)s
         '''), {'event_id': event['id']})
+
+        pg_conn.commit()
 
         handler = event_handler_map[event['entity_type']]
 
@@ -253,6 +265,11 @@ def main():
                             dest='maxwait',
                             type=int,
                             default=32)
+    arg_parser.add_argument('--max-idle-loops',
+                            help='exit after this many idle loops',
+                            dest='max_idle_loops',
+                            type=int,
+                            default=inf)
     args = arg_parser.parse_args()
 
     logger = logging.getLogger()
@@ -272,7 +289,7 @@ def main():
         if sentry_dsn:
             sentry_sdk.init(dsn=sentry_dsn)
 
-    indexer(config, args.maxwait)
+    indexer(config, args.maxwait, max_idle_loops=args.max_idle_loops)
 
 
 if __name__ == '__main__':

pg_conn_wrapper.py
@@ -33,7 +33,6 @@ class PgConnWrapper(object):
         conninfo = psycopg.conninfo.make_conninfo(**self.config['database'])
         self.conn = psycopg.connect(
             conninfo,
-            autocommit=True,
             prepare_threshold=None,
             row_factory=psycopg.rows.dict_row
         )
@@ -57,6 +56,11 @@ class PgConnWrapper(object):
                     'Command failed. Retrying in 30s: ' +
                     f'execute({execute_args})')
                 time.sleep(30)
+
+    def commit(self):
+        if self.conn is None or self.conn.closed:
+            raise Exception('Commit called with no open connection.')
+        return self.conn.commit()
 
     def close(self):
         if self.conn and not self.conn.closed:

run_tests.sh
@@ -6,4 +6,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 ./create_test_db.sh
 
+./tests/test_concurrency.sh
+
 exec coverage run -m unittest discover . "test_*.py"

tests/test_concurrency.sh
@@ -1,0 +1,63 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+read -r -d '' SQL <<'EOF'
+SET client_min_messages TO WARNING;
+INSERT INTO artwork_indexer.event_queue (id, entity_type, action, message, created)
+     VALUES (1, 'release', 'noop', '{"run": 1}', NOW() - interval '2 minutes'),
+            (2, 'release', 'noop', '{"run": 2}', NOW() - interval '1 minute');
+EOF
+
+psql -U musicbrainz -d musicbrainz_test_artwork_indexer -c "$SQL" -q > /dev/null
+
+run_indexer() {
+    python indexer.py \
+        --max-wait=1 \
+        --max-idle-loops=1 \
+        --config=config.tests.ini 2>&1
+}
+
+run_indexer > /tmp/a26a73c_run1_output &
+run1_pid=$!
+
+# This is paired with `time.sleep(0.25)` in indexer.py. That sleep duration
+# is divided by two here in order to
+#  1. give run #1 enough time to select an event (which should lock it), and
+#  2. give run #2 enough time to select an event while run #1 still holds a
+#     lock on the first event (or at least that's what we're verifying).
+sleep 0.125
+
+run_indexer > /tmp/a26a73c_run2_output &
+run2_pid=$!
+
+wait "$run1_pid" "$run2_pid"
+
+run1_id1_completion_count="$(cat /tmp/a26a73c_run1_output | grep -Fo 'Event id=1 completed succesfully' | wc -l)"
+run2_id1_completion_count="$(cat /tmp/a26a73c_run2_output | grep -Fo 'Event id=1 completed succesfully' | wc -l)"
+
+run1_id2_completion_count="$(cat /tmp/a26a73c_run1_output | grep -Fo 'Event id=2 completed succesfully' | wc -l)"
+run2_id2_completion_count="$(cat /tmp/a26a73c_run2_output | grep -Fo 'Event id=2 completed succesfully' | wc -l)"
+
+id1_completion_count="$(( run1_id1_completion_count +  run2_id1_completion_count ))"
+id2_completion_count="$(( run1_id2_completion_count +  run2_id2_completion_count ))"
+
+rm /tmp/a26a73c*
+
+if [[ $id1_completion_count -gt 1 ]]; then
+    echo 'ERROR: Event id=1 was processed more than once'
+    exit 1
+fi
+
+if [[ $id2_completion_count -gt 1 ]]; then
+    echo 'ERROR: Event id=2 was processed more than once'
+    exit 1
+fi
+
+read -r -d '' SQL <<'EOF'
+SET client_min_messages TO WARNING;
+TRUNCATE artwork_indexer.event_queue CASCADE;
+SELECT setval('artwork_indexer.event_queue_id_seq', 1, FALSE);
+EOF
+
+psql -U musicbrainz -d musicbrainz_test_artwork_indexer -c "$SQL" -q > /dev/null